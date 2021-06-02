e2a807d309300a7375017177141116acf3ee46ca
Deanna Castano <paisitadc8@gmail.com>
2021-06-02 16:28:20 +0000

    adding footer map

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,17 +2,25 @@ function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 12,
         center: {
-            lat:  40.767266,
+            lat: 40.767266,
             lng: -73.976431
         }
     });
-   
+
     let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    let locations = [
-        { lat: 40.703621, lng: -74.012149 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+    let locations = [{
+            lat: 40.703621,
+            lng: -74.012149
+        },
+        {
+            lat: 41.084045,
+            lng: -73.874245
+        },
+        {
+            lat: 40.754932,
+            lng: -73.984016
+        }
     ];
 
     let markers = locations.map(function(location, i) {
@@ -22,25 +30,29 @@ function initMap() {
         });
     });
     //adds the marker image for our map, balso going to create them in a cluster if they're close together using that clusterer library that was already loaded
-    let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    let markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
 }
+
+
+//footermap
 
 function initFooterMap() {
     let map = new google.maps.Map(document.getElementById("FooterMap"), {
-        zoom: 2,
+        zoom: 14,
         center: {
-            lat:  40.767266,
-            lng: -73.976431
+            lat: 40.736662,
+            lng: -73.996238
         }
     });
-   
+
     let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    let locations = [
-        { lat: 40.703621, lng: -74.012149 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
+    let locations = [{
+        lat: 40.736662,
+        lng: -73.996238
+    }];
 
     let markers = locations.map(function(location, i) {
         return new google.maps.Marker({
@@ -49,6 +61,7 @@ function initFooterMap() {
         });
     });
     //adds the marker image for our map, balso going to create them in a cluster if they're close together using that clusterer library that was already loaded
-    let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    let markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
 }
-
