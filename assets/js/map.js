9c939d9f64e07cb1d6add40b5be07dbd1c27b4d9
Deanna Castano <paisitadc8@gmail.com>
2021-06-02 17:34:52 +0000

    added marker window to footer map

--- assets/js/map.js
+++ assets/js/map.js
@@ -38,30 +38,48 @@ function initMap() {
 
 //footermap
 
-function initFooterMap() {
+/*function initFooterMap() {
     let map = new google.maps.Map(document.getElementById("FooterMap"), {
         zoom: 14,
         center: {
             lat: 40.736662,
             lng: -73.996238
         }
-    });
-
-    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    let locations = [{
+    });*/
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+function initFooterMap() {
+    const uluru = {
         lat: 40.736662,
         lng: -73.996238
-    }];
-
-    let markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
+    };
+    const map = new google.maps.Map(document.getElementById("FooterMap"), {
+        zoom: 14,
+        center: uluru,
     });
-    //adds the marker image for our map, balso going to create them in a cluster if they're close together using that clusterer library that was already loaded
-    let markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    const contentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+        '<div id="bodyContent">' +
+        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+        "sandstone rock formation in the southern part of the " +
+        "Northern Territory, central Australia.</p>" +
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+        "(last visited June 22, 2009).</p>" +
+        "</div>" +
+        "</div>";
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map,
+        title: "Uluru (Ayers Rock)",
+    });
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
     });
 }
