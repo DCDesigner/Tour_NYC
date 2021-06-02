b973dad5da8530fc69855e4412ee538d1fa8c3fc
Deanna Castano <paisitadc8@gmail.com>
2021-06-02 14:40:20 +0000

    adding map zoom and coordinates

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 12,
         center: {
             lat:  40.767266,
             lng: -73.976431
@@ -21,6 +21,30 @@ function initMap() {
             label: labels[i % labels.length]
         });
     });
+
+    function initFooterMap() {
+        var map = new google.maps.Map(document.getElementById("FooterMap"), {
+            zoom: 12,
+            center: {
+                lat:  40.767266,
+                lng: -73.976431
+            }
+        });
+       
+        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+        var locations = [
+            { lat: 40.703621, lng: -74.012149 },
+            { lat: 41.084045, lng: -73.874245 },
+            { lat: 40.754932, lng: -73.984016 }
+        ];
+    
+        var markers = locations.map(function(location, i) {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        });
     //adds the marker image for our map, balso going to create them in a cluster if they're close together using that clusterer library that was already loaded
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
